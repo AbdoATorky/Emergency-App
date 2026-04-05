a255d0d0eb844f4f5b8ab6ee3b0392ad2864d4c9
AbdoATorky <abdalsalamtorky@gmail.com>
2026-04-05 19:59:50 +0200

    Initialize RecyclerView in MainActivity and format EmergencyAdapter code

--- app/src/main/java/com/abs/emergencyapp/EmergencyAdapter.kt
+++ app/src/main/java/com/abs/emergencyapp/EmergencyAdapter.kt
@@ -10,31 +10,31 @@ import androidx.cardview.widget.CardView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 
-class EmergencyAdapter(val activity: Activity , val emergencies : ArrayList<Emergency>):
-    RecyclerView.Adapter<EmergencyAdapter.MVH>(){
+class EmergencyAdapter(val activity: Activity, val emergencies: List<Emergency>) :
+    RecyclerView.Adapter<EmergencyAdapter.MVH>() {
 
-    class MVH (v : View): RecyclerView.ViewHolder(v) {
-        val parent : CardView = v.findViewById(R.id.parent)
-        val iv : ImageView = v.findViewById(R.id.image)
-        val tv : TextView = v.findViewById(R.id.tv)
+    class MVH(v: View) : RecyclerView.ViewHolder(v) {
+        val parent: CardView = v.findViewById(R.id.parent)
+        val iv: ImageView = v.findViewById(R.id.image)
+        val tv: TextView = v.findViewById(R.id.tv)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MVH {
         val view = activity.layoutInflater
-            .inflate(R.layout.emergency_list_item,parent,false)
+            .inflate(R.layout.emergency_list_item, parent, false)
         return MVH(view)
     }
 
     override fun onBindViewHolder(holder: MVH, position: Int) {
         holder.iv.setImageResource(emergencies[position].pic)
-        holder.tv.text= emergencies[position].name
+        holder.tv.text = emergencies[position].name
         holder.parent.setOnClickListener {
-            val i = Intent(Intent.ACTION_DIAL,"tel:${emergencies[position].number}".toUri())
+            val i = Intent(Intent.ACTION_DIAL, "tel:${emergencies[position].number}".toUri())
             activity.startActivity(i)
         }
     }
 
-    override fun getItemCount() =emergencies.size
+    override fun getItemCount() = emergencies.size
 
 
 }
--- app/src/main/java/com/abs/emergencyapp/MainActivity.kt
+++ app/src/main/java/com/abs/emergencyapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +36,14 @@ class MainActivity : AppCompatActivity() {
                 "122",
             )
         )
+
+        val emergencyRV : RecyclerView = findViewById(R.id.rv)
+        emergencyRV.setHasFixedSize(true)
+
+        val adapter = EmergencyAdapter(this,emergencies)
+        emergencyRV.adapter=adapter
+
+
 
     }
 }
